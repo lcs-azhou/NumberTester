d6ff09f4e5b8e77b65f54ad5d1f18aa2be4fd637
Ansheng Zhou <azhou@lcs.on.ca>
2024-09-23 12:11:38 -0400

    first try, stuck on assembling them together

--- NumberTester/main.swift
+++ NumberTester/main.swift
@@ -7,5 +7,71 @@
 
 import Foundation
 
-print("Hello, World!")
+//func getInput(prompt: String) -> Int {
+//    
+//    while true {
+//        print(prompt)
+//        
+//        if let input = readLine(), let number = Int(input) {
+//            
+//            if number < 1 || number > 32500 {
+//                print("Try again with e a number that is more or equals to 1, and less than 32500")
+//            } else {
+//                return number
+//            }
+//        }
+//    }
+//}
 
+func getInput(prompt: String) -> Int {
+    
+    while true {
+        print(prompt)
+        
+        if let input = readLine(), let number = Int(input), number > 1 || number < 32500{
+            
+            return number
+            
+        } else {
+            print("Try again with e a number that is more or equals to 1, and less than 32500")
+        }
+    }
+}
+
+func isDivisor(dividend: Int, divisor: Int) -> Bool {
+    return dividend % divisor == 0
+}
+
+func isProperDivisor(number: Int) -> Bool {
+    var properDivisorCount = 0
+    
+    for i in 1...number - 1 {
+        if isDivisor(dividend: number, divisor: i) {
+            properDivisorCount += 1
+        }
+    }
+    print(properDivisorCount)
+    return properDivisorCount == number
+}
+
+func getAllDivisor(start: Int, end: Int) -> [Int] {
+    var divisorNumbers = [Int]()
+    
+    for number in start...end {
+        if isProperDivisor(number: number) {
+            divisorNumbers.append(number)
+        }
+    }
+    
+    return divisorNumbers
+}
+
+
+func mainFunc() {
+    let number = getInput(prompt: "Give me a number.")
+    
+    
+    
+}
+
+mainFunc()
